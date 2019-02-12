44c3dd5ca0bb0a1ec5ad412fe1c4a5e694d9a82b
sianbrac <sian.brace@ons.gov.uk>
2019-02-12 16:00:54 +0000

    Change message from Goodbye to Hello

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__, instance_relative_config=True)
 
+
 @app.route('/')
 def hello_world():
     recipient = os.getenv("RECIPIENT", "world")
 
-    return jsonify({"message": "Goodbye", "recipient": recipient})
+    return jsonify({"message": "Hello", "recipient": recipient, "result": "OK"})
+
 
 if __name__ == "__main__":
     app.run()
